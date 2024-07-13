49af98d7cc75db1caf4e7ab421aabb6d1c1386b6
Vitor Pio <vitormarquespio@gmail.com>
2024-07-13 12:22:00 -0300

    random rock start

---
+++ Assets/Scripts/rock/RandomRockStart.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRockStart : MonoBehaviour
+{
+    public List<Texture2D> RockTextures;
+
+
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    void Start()
+    {
+        int randomIndex = UnityEngine.Random.Range(0, RockTextures.Count);
+        spriteRenderer.sprite = Sprite.Create(RockTextures[randomIndex], new Rect(0, 0, RockTextures[randomIndex].width, RockTextures[randomIndex].height), new Vector2(0.5f, 0.5f));
+    }
+
+}
