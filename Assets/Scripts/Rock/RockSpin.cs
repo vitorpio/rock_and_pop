3514d85fcdb0b66ea43c6518a7c2e6e1e5f2af45
Vitor Pio <vitormarquespio@gmail.com>
2024-07-16 20:03:04 -0300

    change rock rotation depending on angle

--- Assets/Scripts/Rock/RockSpin.cs
+++ Assets/Scripts/Rock/RockSpin.cs
@@ -1,24 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class RockSpin : MonoBehaviour
 {
     private GameController gameController;
     private ForceController forceController;
+    private AimController aimController;
     private float spinSpeed = 1;
 
     void Awake()
     {
         gameController = FindObjectOfType<GameController>();
         forceController = FindObjectOfType<ForceController>();
+        aimController = FindObjectOfType<AimController>();
     }
 
     void FixedUpdate()
     {
         if (gameController.CurrentGameState == GameState.WaitingForNextTurn)
         {
-            transform.Rotate(0, 0, 1 * forceController.forceMultiplier);
+            transform.Rotate(0, 0, (aimController.Angle >= 0 ? 1 : -1) * forceController.forceMultiplier);
         }
     }
 }
