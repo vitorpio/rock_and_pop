be1f0686adcca8672dfbeb3b78c047b52a913ff5
Vitor Pio <vitormarquespio@gmail.com>
2024-08-03 14:20:23 -0300

    Refactor BallonColliderController

--- Assets/Scripts/Ballon/BallonColliderController.cs
+++ Assets/Scripts/Ballon/BallonColliderController.cs
@@ -1,13 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class BallonColliderController : MonoBehaviour
 {
-    public int points;
-
     private GameController gameController;
+
+    public int Points;
 
     void Awake()
     {
@@ -19,7 +16,7 @@ public class BallonColliderController : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             Destroy(gameObject);
-            gameController.AddPoints(points);
+            gameController.AddPoints(Points);
         }
     }
 
