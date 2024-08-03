2b91fc43d6b164a6d407d158e1fb1abadfb37b81
Vitor Pio <vitormarquespio@gmail.com>
2024-08-03 20:19:49 -0300

    animação balão vermelho

--- Assets/Scripts/Ballon/BallonColliderController.cs
+++ Assets/Scripts/Ballon/BallonColliderController.cs
@@ -3,19 +3,43 @@ using UnityEngine;
 public class BallonColliderController : MonoBehaviour
 {
     private GameController gameController;
+    private PolygonCollider2D polygonCollider;
+    private SpriteRenderer spriteRenderer;
 
     public int Points;
 
     void Awake()
     {
         gameController = FindObjectOfType<GameController>();
+        polygonCollider = GetComponent<PolygonCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    void Update()
+    {
+        UpdatePolygonCollider();
+    }
+
+    void UpdatePolygonCollider()
+    {
+        // Get the sprite's vertices and update the collider points
+        Vector2[] spriteVertices = spriteRenderer.sprite.vertices;
+        Vector2[] colliderPoints = new Vector2[spriteVertices.Length];
+
+        for (int i = 0; i < spriteVertices.Length; i++)
+        {
+            colliderPoints[i] = spriteVertices[i];
+        }
+
+        polygonCollider.points = colliderPoints;
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Destroy(gameObject);
+            Destroy(transform.parent.gameObject);
             gameController.AddPoints(Points);
         }
     }
