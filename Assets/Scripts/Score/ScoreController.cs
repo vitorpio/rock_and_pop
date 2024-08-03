b3f5c6250434527993e967fb4191b88dd45a458a
Vitor Pio <vitormarquespio@gmail.com>
2024-08-03 18:49:05 -0300

    Refactor ScoreController

--- Assets/Scripts/Score/ScoreController.cs
+++ Assets/Scripts/Score/ScoreController.cs
@@ -1,13 +1,11 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreController : MonoBehaviour
 {
+    private readonly string prefix = "Pontuação: ";
+
     private Text scoreText;
-    private string prefix = "Pontuação: ";
 
     void Awake()
     {
