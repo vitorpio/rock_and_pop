7b1eab1c8176e5a94532812b505404a0dd4e653f
Vitor Pio <vitormarquespio@gmail.com>
2024-08-15 20:04:23 -0300

    increase angle

--- Assets/Scripts/Aim/AimController.cs
+++ Assets/Scripts/Aim/AimController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class AimController : MonoBehaviour
 {
-    private readonly float maxAngleShot = 35;
+    private readonly float maxAngleShot = 90;
     private readonly int sensitivity = 100;
 
     private GameController gameController;
