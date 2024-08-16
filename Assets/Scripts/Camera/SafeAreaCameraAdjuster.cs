3055b104d18078b8b55ecb15bc58a5af9d65b22b
Vitor Pio <vitormarquespio@gmail.com>
2024-08-16 17:22:08 -0300

    script to ajust camera to safe area

---
+++ Assets/Scripts/Camera/SafeAreaCameraAdjuster.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class SafeAreaCameraAdjuster : MonoBehaviour
+{
+    private Camera mainCamera;
+    public GameObject UIBack;
+    public GameObject UIFront;
+
+    void Awake()
+    {
+        mainCamera = GetComponent<Camera>();
+        AdjustCameraViewport();
+        UIBack.SetActive(true);
+        UIFront.SetActive(true);
+    }
+
+    void AdjustCameraViewport()
+    {
+        Rect safeArea = Screen.safeArea;
+
+        // Convert safe area from screen space to viewport space
+        Vector2 viewportMin = new(safeArea.xMin / Screen.width, safeArea.yMin / Screen.height);
+        Vector2 viewportMax = new(safeArea.xMax / Screen.width, safeArea.yMax / Screen.height);
+
+        // Set the camera's viewport rect
+        mainCamera.rect = new Rect(viewportMin.x, viewportMin.y, viewportMax.x - viewportMin.x, viewportMax.y - viewportMin.y);
+    }
+
+    void OnRectTransformDimensionsChange()
+    {
+        AdjustCameraViewport();
+    }
+}
