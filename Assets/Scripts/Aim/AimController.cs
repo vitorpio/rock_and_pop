09275e079a2cb84538eb08350830714b9b0c901c
Vitor Pio <vitormarquespio@gmail.com>
2024-08-17 19:03:59 -0300

    justar sensibilidade para celular

--- Assets/Scripts/Aim/AimController.cs
+++ Assets/Scripts/Aim/AimController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class AimController : MonoBehaviour
 {
     private readonly float maxAngleShot = 90;
-    private readonly int sensitivity = 50;
+    private readonly int sensitivity = 20;
 
     private GameController gameController;
     private float lastMouseX;
